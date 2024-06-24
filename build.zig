3a75184ab51f3661ee63f270a763fc413077d79b
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-24 19:39:16 -0300

    feat: Adding include and linking with Raylib

--- build.zig
+++ build.zig
@@ -36,6 +36,11 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.addIncludePath(.{ .path = "/usr/local/include/raylib.h" });
+    exe.addIncludePath(.{ .path = "/usr/local/include/raymath.h" });
+    exe.linkLibC();
+    exe.linkSystemLibrary("raylib");
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
