d1db804ef50212762c652e46d61e3cf92a197d15
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-25 20:01:21 -0300

    feat: Adding mouse button enum on input module

--- src/input/input.zig
+++ src/input/input.zig
@@ -7,25 +7,45 @@ pub const KeyEnum = enum(i32) {
     Right,
 };
 
+pub const MouseBntEnum = enum(i32) {
+    Left,
+    Middle,
+    Right,
+};
+
 pub const Input = struct {
     pub fn isKeyPressed(keyCode: KeyEnum) bool {
-        const RaylibKeyCode = Input.keyEnumToRaylib(keyCode);
-        return Raylib.IsKeyPressed(RaylibKeyCode);
+        const raylibKeyCode = Input.keyEnumToRaylib(keyCode);
+        return Raylib.IsKeyPressed(raylibKeyCode);
     }
 
     pub fn isKeyDown(keyCode: KeyEnum) bool {
-        const RaylibKeyCode = Input.keyEnumToRaylib(keyCode);
-        return Raylib.IsKeyDown(RaylibKeyCode);
+        const raylibKeyCode = Input.keyEnumToRaylib(keyCode);
+        return Raylib.IsKeyDown(raylibKeyCode);
     }
 
     fn keyEnumToRaylib(keyCode: KeyEnum) i32 {
-        const RaylibKeyCode = switch (keyCode) {
+        const raylibKeyCode = switch (keyCode) {
             .Up => Raylib.KEY_W,
             .Down => Raylib.KEY_S,
             .Left => Raylib.KEY_A,
             .Right => Raylib.KEY_D,
         };
 
-        return RaylibKeyCode;
+        return raylibKeyCode;
+    }
+
+    pub fn isMouseBntPressed(mouse: MouseBntEnum) bool {
+        const raylibMouseCode = Input.mouseEnumToRaylib(mouse);
+        return Raylib.IsMouseButtonPressed(raylibMouseCode);
+    }
+
+    fn mouseEnumToRaylib(mouse: MouseBntEnum) i32 {
+        const raylibMouseBntCode = switch (mouse) {
+            .Left => Raylib.MOUSE_BUTTON_LEFT,
+            .Right => Raylib.MOUSE_BUTTON_RIGHT,
+            .Middle => Raylib.MOUSE_BUTTON_MIDDLE,
+        };
+        return raylibMouseBntCode;
     }
 };
