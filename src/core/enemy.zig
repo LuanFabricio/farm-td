ded4f1bf1a90ec017743897da6069840e016851e
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-26 20:32:17 -0300

    feat: Adding basic sender methods (for observer)

--- src/core/enemy.zig
+++ src/core/enemy.zig
@@ -1,6 +1,12 @@
+const std = @import("std");
+
+const ArrayList = std.ArrayList;
+const PageAllocator = std.heap.page_allocator;
+
 const utils = @import("../utils/utils.zig");
 
 const Entity = @import("entity.zig").Entity;
+const Turret = @import("turret.zig").Turret;
 
 pub const DEFAULT_COLOR = utils.Color{
     .r = 0x19,
@@ -11,10 +17,26 @@ pub const DEFAULT_COLOR = utils.Color{
 
 pub const Enemy = struct {
     entity: Entity,
+    turrets: ArrayList(*Turret),
 
     pub fn init(box: utils.Rectangle) Enemy {
         return Enemy{
             .entity = Entity.defaultEnemy(box),
+            .turrets = ArrayList(*Turret).init(PageAllocator),
         };
+    }
+
+    pub fn deinit(self: *Enemy) void {
+        self.turrets.deinit();
+    }
+
+    pub fn addObserver(self: *Enemy, observer: *Turret) !void {
+        try self.turrets.append(observer);
+    }
+
+    pub fn notifyAll(self: *Enemy) void {
+        for (self.turrets.items) |turret| {
+            turret.observer(&self.entity);
+        }
     }
 };
