6b7eaa767d4889ddc2e2944ff0c332d6738fffdc
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-29 01:56:53 -0300

    feat: Adding addItem method for Grid struct
    Adding addItem method for Grid struct and, creating newTurret and
    newEnemy methods for GridItem struct

--- src/core/grid.zig
+++ src/core/grid.zig
@@ -1,4 +1,4 @@
-const GridItemEnum = enum(i3) {
+pub const GridItemEnum = enum(i3) {
     Empty,
     Turret,
     Enemy,
@@ -12,6 +12,20 @@ pub const GridItem = struct {
         return GridItem{
             .data = null,
             .itemType = GridItemEnum.Empty,
+        };
+    }
+
+    fn newTurret(turret: *anyopaque) GridItem {
+        return GridItem{
+            .data = turret,
+            .itemType = GridItemEnum.Turret,
+        };
+    }
+
+    fn newEnemy(enemy: *anyopaque) GridItem {
+        return GridItem{
+            .data = enemy,
+            .itemType = GridItemEnum.Enemy,
         };
     }
 };
@@ -28,5 +42,17 @@ pub const Grid = struct {
             .height = height,
             .items = &[_]GridItem{GridItem.newEmpty()} ** len,
         };
+    }
+
+    pub fn addItem(self: *Grid, x: usize, y: usize, itemType: GridItemEnum, item: *anyopaque) void {
+        const idx = y * self.width + x;
+
+        const gridItem = switch (itemType) {
+            GridItemEnum.Turret => GridItem.newTurret(item),
+            GridItemEnum.Enemy => GridItem.newEnemy(item),
+            GridItemEnum.Empty => GridItem.newEmpty(),
+        };
+
+        self.items[idx] = gridItem;
     }
 };
