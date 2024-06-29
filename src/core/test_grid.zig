9b4c42afa738d198e9cba19171540aa1792ff5d8
LuanFabricio <luanlimaleite@hotmail.com>
2024-06-29 01:58:20 -0300

    test: Adding test for the method addItem

--- src/core/test_grid.zig
+++ src/core/test_grid.zig
@@ -1,13 +1,36 @@
 const expect = @import("std").testing.expect;
 
-const grid = @import("grid.zig");
+const utils = @import("../utils/utils.zig");
+const Turret = @import("turret.zig").Turret;
 
+const grid = @import("grid.zig");
 const Grid = grid.Grid;
+const GridItemEnum = grid.GridItemEnum;
 
 test "It should create a new Grid" {
-    const g = Grid.new(32, 32);
+    const w = 32;
+    const h = 32;
+    const g = Grid.new(w, h);
 
-    try expect(g.width == 32);
-    try expect(g.height == 32);
-    try expect(g.items.len == 32 * 32);
+    const len = w * h;
+    try expect(g.width == w);
+    try expect(g.height == h);
+    try expect(g.items.len == len);
+}
+
+test "It should add an item" {
+    var g = Grid.new(32, 32);
+
+    var t = Turret.new(.{
+        .x = 42,
+        .y = 42,
+        .w = 32,
+        .h = 32,
+    });
+
+    g.addItem(3, 3, GridItemEnum.Turret, @as(*anyopaque, @ptrCast(&t)));
+
+    const idx = 3 * g.width + 3;
+    const item = g.items[idx];
+    try expect(item.itemType == GridItemEnum.Turret);
 }
