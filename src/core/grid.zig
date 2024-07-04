da61604ca0bf37ec4acde05ee5b56c2abcb4c51a
LuanFabricio <luanlimaleite@hotmail.com>
2024-07-04 19:00:37 -0300

    fix: Moving grid items to option and removing empty enum

--- src/core/grid.zig
+++ src/core/grid.zig
@@ -10,13 +10,11 @@ const Enemy = @import("enemy.zig").Enemy;
 const Entity = @import("entity.zig").Entity;
 
 pub const GridItemEnum = enum(u8) {
-    empty = 0,
-    enemy = 1,
-    turret = 2,
+    enemy = 0,
+    turret = 1,
 };
 
 pub const GridItem = union(GridItemEnum) {
-    empty: u8,
     enemy: *Enemy,
     turret: *Turret,
 };
@@ -24,7 +22,7 @@ pub const GridItem = union(GridItemEnum) {
 pub const Grid = struct {
     width: usize,
     height: usize,
-    items: ArrayList(GridItem),
+    items: ArrayList(?GridItem),
 
     pub fn init(width: usize, height: usize) !Grid {
         const len = width * height;
@@ -32,24 +30,28 @@ pub const Grid = struct {
         var g = Grid{
             .width = width,
             .height = height,
-            .items = ArrayList(GridItem).init(allocator),
+            .items = ArrayList(?GridItem).init(allocator),
         };
         try g.items.resize(len);
 
         var i: usize = 0;
         while (i < len) : (i += 1) {
-            g.items.items[i] = GridItem{ .empty = 0 };
+            g.items.items[i] = null;
         }
 
         return g;
     }
 
     pub fn deinit(self: *const Grid) void {
-        for (self.items.items) |item_ptr| {
-            switch (item_ptr) {
-                GridItemEnum.turret => |turret| allocator.destroy(turret),
-                GridItemEnum.enemy => |enemy| allocator.destroy(enemy),
-                GridItemEnum.empty => {},
+        for (self.items.items) |item| {
+            if (item) |item_ptr| {
+                switch (item_ptr) {
+                    GridItemEnum.turret => |turret| allocator.destroy(turret),
+                    GridItemEnum.enemy => |enemy| {
+                        enemy.deinit();
+                        allocator.destroy(enemy);
+                    },
+                }
             }
         }
 
@@ -70,13 +72,12 @@ pub const Grid = struct {
         const gridItem = switch (itemType) {
             .turret => GridItem{ .turret = @as(*Turret, @ptrCast(@alignCast(item))) },
             .enemy => GridItem{ .enemy = @as(*Enemy, @ptrCast(@alignCast(item))) },
-            .empty => GridItem{ .empty = 0 },
         };
 
         self.items.items[idx] = gridItem;
     }
 
-    pub fn getItem(self: *Grid, x: usize, y: usize) !GridItem {
+    pub fn getItem(self: *Grid, x: usize, y: usize) !?GridItem {
         const idx = self.xyToIndex(x, y);
 
         if (idx > self.items.items.len) {
