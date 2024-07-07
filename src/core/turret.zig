e0a696acc3f31599390cd5983d75e1e7fe6be7d5
LuanFabricio <luanlimaleite@hotmail.com>
2024-07-07 16:54:45 -0300

    fix: Removing turret box position

--- src/core/turret.zig
+++ src/core/turret.zig
@@ -20,22 +20,22 @@ pub const Turret = struct {
     attackTime: i64,
 
     // Create on the stack
-    pub fn new(box: utils.Rectangle) Turret {
+    pub fn new() Turret {
         const attackTime = timestamp();
 
         return Turret{
-            .entity = Entity.defaultTurret(box),
+            .entity = Entity.defaultTurret(),
             .attackDelay = 1,
             .attackTime = attackTime,
         };
     }
 
     // Allocate on the heap
-    pub fn init(box: utils.Rectangle) !*Turret {
+    pub fn init() !*Turret {
         var turretPtr = try Allocator.create(Turret);
         const attackTime = timestamp();
 
-        turretPtr.entity = Entity.defaultTurret(box);
+        turretPtr.entity = Entity.defaultTurret();
         turretPtr.attackDelay = 1;
         turretPtr.attackTime = attackTime;
 
@@ -48,22 +48,22 @@ pub const Turret = struct {
         self.attackDelay = other.attackDelay;
     }
 
-    pub fn observer(self: *Turret, entity: *Entity) void {
-        const enemyCenter = entity.box.getCenter();
-        const selfCenter = self.entity.box.getCenter();
-        const dist = selfCenter.calcDist(&enemyCenter);
+    pub fn observer(_: *Turret, _: *Entity) void {
+        // const enemyCenter = entity.box.getCenter();
+        // const selfCenter = self.entity.box.getCenter();
+        // const dist = selfCenter.calcDist(&enemyCenter);
 
-        const range = self.entity.status.range;
+        // const range = self.entity.status.range;
 
-        // NOTE: Maybe, in the future, create two types of turrets radius and line turrets.
-        // Where the radius turret shots based on the radius range (a circle)
-        // and the line turrets shot based on the line in the grid
-        if (dist <= range) {
-            const now = timestamp();
-            if (now >= self.attackTime) {
-                entity.status.health -= self.entity.status.attack;
-                self.attackTime = now + self.attackDelay;
-            }
-        }
+        // // NOTE: Maybe, in the future, create two types of turrets radius and line turrets.
+        // // Where the radius turret shots based on the radius range (a circle)
+        // // and the line turrets shot based on the line in the grid
+        // if (dist <= range) {
+        //     const now = timestamp();
+        //     if (now >= self.attackTime) {
+        //         entity.status.health -= self.entity.status.attack;
+        //         self.attackTime = now + self.attackDelay;
+        //     }
+        // }
     }
 };
