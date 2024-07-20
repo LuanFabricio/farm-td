746f5101fb5ba85c3c6625c64200435fee162267
LuanFabricio <luanlimaleite@hotmail.com>
2024-07-20 12:28:30 -0300

    test: Adding a test case for Farm module

--- src/core/test_farm.zig
+++ src/core/test_farm.zig
@@ -6,7 +6,15 @@ const Allocator = std.heap.page_allocator;
 const farmImport = @import("farm.zig");
 const Farm = farmImport.Farm;
 
-test "Should get gold and update the timer" {
+test "Should start in cooldown" {
+    const farmItem = try Farm.init(10, 10, 1200);
+    defer Allocator.destroy(farmItem);
+
+    const now = timestamp();
+    try expect(now < farmItem.goldTime);
+}
+
+test "Should get gold and update the goldTime with delay" {
     const goldGain = 42;
     const delay = 2;
     var farmItem = try Farm.init(10, goldGain, delay);
