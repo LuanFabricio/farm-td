3d336d0fab5d8f4c3ef437dbd7ca7074f6cc9351
LuanFabricio <luanlimaleite@hotmail.com>
2024-07-23 20:09:12 -0300

    test: Adding test case for attack method on Enemy module

--- src/core/test_enemy.zig
+++ src/core/test_enemy.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const timestamp = std.time.timestamp;
 const expect = std.testing.expect;
 
 const Allocator = std.heap.page_allocator;
@@ -8,6 +9,9 @@ const utils = @import("../utils/utils.zig");
 const enemyImport = @import("enemy.zig");
 const Enemy = enemyImport.Enemy;
 const EnemySpawner = enemyImport.EnemySpawner;
+
+const entityImport = @import("entity.zig");
+const Entity = entityImport.Entity;
 
 fn compareEnemies(e1: Enemy, e2: Enemy) !void {
     try expect(e1.entity.status.isEqual(e2.entity.status));
@@ -51,4 +55,72 @@ test "Should spawn an enemy and update the spawnTime" {
     try expect(e != null);
 
     try expect(spawner.spawnTime > oldAttackTime);
+}
+
+test "Should attack another entity" {
+    const baseBox = utils.Rectangle{
+        .x = 42,
+        .y = 42,
+        .w = 42,
+        .h = 42,
+    };
+
+    const enemy = try Enemy.init(baseBox);
+    defer Allocator.destroy(enemy);
+    defer enemy.deinit();
+
+    var entity = Entity{
+        .status = .{
+            .range = 0,
+            .attack = 0,
+            .health = 1000,
+        },
+        .defaultStatus = .{
+            .range = 0,
+            .attack = 0,
+            .health = 1000,
+        },
+    };
+
+    const oldHp = entity.status.health;
+    enemy.attackEntity(&entity);
+
+    try expect(entity.status.health < oldHp);
+}
+
+test "Should validate attack cooldown and enemy range" {
+    const baseBox = utils.Rectangle{
+        .x = 42,
+        .y = 42,
+        .w = 42,
+        .h = 42,
+    };
+    const enemyCenter = baseBox.getCenter();
+
+    const enemy = try Enemy.init(baseBox);
+    defer Allocator.destroy(enemy);
+    defer enemy.deinit();
+
+    enemy.entity.status.range = 10;
+    var p1 = enemyCenter;
+    p1.x += 10;
+    p1.y += 10;
+
+    // Should not attack if the enemy is far and attack is on cooldown
+    try expect(!enemy.shouldAttack(p1));
+
+    enemy.attackTime = timestamp();
+    // Should not attack if the enemy is far and attack isnt on cooldown
+    try expect(!enemy.shouldAttack(p1));
+
+    var p2 = enemyCenter;
+    p2.x += 3;
+    p2.y += 3;
+    enemy.attackTime = timestamp() + 3600;
+    // Should not attack if the enemy near and attack is on cooldown
+    try expect(!enemy.shouldAttack(p2));
+
+    enemy.attackTime = timestamp();
+    // Should attack if the enemy near and attack is on cooldown
+    try expect(enemy.shouldAttack(p2));
 }
