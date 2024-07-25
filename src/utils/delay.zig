78bfc050fd3eb389638210ab619354875a47ae69
LuanFabricio <luanlimaleite@hotmail.com>
2024-07-24 23:04:01 -0300

    feat: Adding Delay struct on utils module

---
+++ src/utils/delay.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const timestamp = std.time.timestamp;
+
+pub const Delay = struct {
+    const This = @This();
+    delay: i64,
+    timer: i64,
+
+    pub fn new(delay: i64, startOnCooldown: bool) This {
+        const now = timestamp();
+        return This{
+            .delay = delay,
+            .timer = now + if (startOnCooldown) delay else 0,
+        };
+    }
+
+    pub fn onCooldown(self: *const This) bool {
+        const now = timestamp();
+        return self.timer <= now;
+    }
+
+    pub fn applyDelay(self: *This) void {
+        const now = timestamp();
+        self.timer = now + self.delay;
+    }
+};
