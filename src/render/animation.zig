e5559188e21ce6557164af008e28a62a4352b26b
LuanFabricio <luanlimaleite@hotmail.com>
2024-08-03 00:09:07 -0300

    fix: Commenting old animation code and renaming the new animation implementation

--- src/render/animation.zig
+++ src/render/animation.zig
@@ -16,7 +16,7 @@ const SpriteSheet = spriteImport.SpriteSheet;
 
 // const _Animation = struct { };
 
-pub fn _Animation(
+fn _Animation(
     value_type: type,
     initFn: fn ([:0]const u8, [2]usize, [2]usize, utils.Point) value_type,
     deinitFn: fn (sprites: *value_type) void,
@@ -85,21 +85,21 @@ pub fn _Animation(
     };
 }
 
-fn _initAnimation1(filename: [:0]const u8, sprtSize: [2]usize, gridSize: [2]usize, sprtPadding: utils.Point) SpriteSheet {
+fn _initAnimationSpritesheet(filename: [:0]const u8, sprtSize: [2]usize, gridSize: [2]usize, sprtPadding: utils.Point) SpriteSheet {
     return SpriteSheet.load_sprite_sheet(filename, sprtSize[0], sprtSize[1], gridSize[0], gridSize[1], sprtPadding);
 }
 
-fn _deinitAnimation1(sprites: *SpriteSheet) void {
+fn _deinitAnimationSpritesheet(sprites: *SpriteSheet) void {
     sprites.unload_sprite_sheet();
 }
 
-fn _maxSpritesFn1(sprites: SpriteSheet) usize {
+fn _maxSpritesFnSpritesheet(sprites: SpriteSheet) usize {
     return sprites.gridRows * sprites.gridCols;
 }
 
-pub const Animation1 = _Animation(SpriteSheet, _initAnimation1, _deinitAnimation1, _maxSpritesFn1);
+pub const AnimationSpritesheet = _Animation(SpriteSheet, _initAnimationSpritesheet, _deinitAnimationSpritesheet, _maxSpritesFnSpritesheet);
 
-fn _initAnimation2(filename: [:0]const u8, _: [2]usize, gridSize: [2]usize, _: utils.Point) ArrayList(Sprite) {
+fn _initAnimationSprites(filename: [:0]const u8, _: [2]usize, gridSize: [2]usize, _: utils.Point) ArrayList(Sprite) {
     var sprites = ArrayList(Sprite).init(Allocator);
 
     // Only uses first item
@@ -113,138 +113,138 @@ fn _initAnimation2(filename: [:0]const u8, _: [2]usize, gridSize: [2]usize, _: u
     return sprites;
 }
 
-fn _deinitAnimation2(sprites: *ArrayList(Sprite)) void {
+fn _deinitAnimationSprites(sprites: *ArrayList(Sprite)) void {
     for (sprites.items) |*sprt| {
         sprt.unload_texture();
     }
     sprites.deinit();
 }
 
-fn _maxSpritesFn2(sprites: ArrayList(Sprite)) usize {
+fn _maxSpritesFnSprites(sprites: ArrayList(Sprite)) usize {
     return sprites.items.len;
 }
 
-pub const Animation2 = _Animation(ArrayList(Sprite), _initAnimation2, _deinitAnimation2, _maxSpritesFn2);
+pub const AnimationSprites = _Animation(ArrayList(Sprite), _initAnimationSprites, _deinitAnimationSprites, _maxSpritesFnSprites);
 
-pub const Animation = struct {
-    const This = @This();
-    sprites: ArrayList(Sprite),
-    currentSprite: usize,
-    delay: Delay,
-    reverseToLoop: bool,
-    onReverse: bool,
-
-    pub fn init(filepath: []const u8, frames: usize, reverseToLoop: bool) !This {
-        var sprites = ArrayList(Sprite).init(Allocator);
-
-        for (1..(frames + 1)) |idx| {
-            const file = try std.fmt.allocPrintZ(Allocator, "{s}{d}.png", .{ filepath, idx });
-            defer Allocator.free(file);
-            try sprites.append(Sprite.load_texture(file));
-        }
-
-        return This{
-            .sprites = sprites,
-            .currentSprite = 0,
-            .delay = Delay.new(500, true),
-            .reverseToLoop = reverseToLoop,
-            .onReverse = false,
-        };
-    }
-
-    pub fn deinit(self: *const This) void {
-        for (self.sprites.items) |sprite| {
-            sprite.unload_texture();
-        }
-        self.sprites.deinit();
-    }
-
-    pub fn nextSprite(self: *This) void {
-        if (self.delay.onCooldown()) return;
-
-        if (self.onReverse) {
-            self.applyReverse();
-        } else {
-            self.currentSprite += 1;
-        }
-
-        const shouldReset = self.currentSprite >= self.sprites.items.len;
-        const shouldReverse = !self.onReverse and shouldReset;
-        if (self.reverseToLoop and shouldReverse) {
-            self.onReverse = true;
-            self.currentSprite -= 1;
-        } else if (!self.reverseToLoop and shouldReset) self.currentSprite = 0;
-
-        self.delay.applyDelay();
-    }
-
-    fn applyReverse(self: *This) void {
-        if (self.currentSprite != 0) {
-            self.currentSprite -= 1;
-            return;
-        }
-        self.currentSprite = 0;
-        self.onReverse = false;
-    }
-};
-
-pub const AnimationSpriteSheet = struct {
-    const This = @This();
-    spriteSheet: SpriteSheet,
-    delay: Delay,
-    count: usize,
-    reverseToLoop: bool,
-    onReverse: bool,
-
-    pub fn init(filepath: [:0]const u8, spriteSize: [2]usize, grid: [2]usize, padding: utils.Point, reverseToLoop: bool) This {
-        const spriteSheet = SpriteSheet.load_sprite_sheet(filepath, spriteSize[0], spriteSize[1], grid[0], grid[1], padding);
-
-        return This{
-            .spriteSheet = spriteSheet,
-            .delay = Delay.new(500, true),
-            .count = 0,
-            .reverseToLoop = reverseToLoop,
-            .onReverse = false,
-        };
-    }
-
-    pub fn deinit(self: *This) void {
-        self.spriteSheet.unload_sprite_sheet();
-    }
-
-    pub fn nextSprite(self: *This) void {
-        if (self.delay.onCooldown()) return;
-
-        if (self.onReverse) {
-            self.applyReverse();
-        } else {
-            self.count += 1;
-        }
-
-        const shouldReset = self.count >= (self.spriteSheet.gridRows * self.spriteSheet.gridCols);
-        const shouldReverse = !self.onReverse and shouldReset;
-        if (self.reverseToLoop and shouldReverse) {
-            self.onReverse = true;
-            self.count -= 1;
-        } else if (!self.reverseToLoop and shouldReset) self.count = 0;
-
-        self.delay.applyDelay();
-    }
-
-    fn applyReverse(self: *This) void {
-        if (self.count != 0) {
-            self.count -= 1;
-            return;
-        }
-
-        self.count = 0;
-        self.onReverse = false;
-    }
-
-    pub fn draw(self: *const This, render: *const Render, position: utils.Point) void {
-        const row = self.count / self.spriteSheet.gridRows;
-        const col = self.count / self.spriteSheet.gridCols;
-
-        render.drawSpriteSheet(position, self.spriteSheet, row, col);
-    }
-};
+// pub const Animation = struct {
+//     const This = @This();
+//     sprites: ArrayList(Sprite),
+//     currentSprite: usize,
+//     delay: Delay,
+//     reverseToLoop: bool,
+//     onReverse: bool,
+//
+//     pub fn init(filepath: []const u8, frames: usize, reverseToLoop: bool) !This {
+//         var sprites = ArrayList(Sprite).init(Allocator);
+//
+//         for (1..(frames + 1)) |idx| {
+//             const file = try std.fmt.allocPrintZ(Allocator, "{s}{d}.png", .{ filepath, idx });
+//             defer Allocator.free(file);
+//             try sprites.append(Sprite.load_texture(file));
+//         }
+//
+//         return This{
+//             .sprites = sprites,
+//             .currentSprite = 0,
+//             .delay = Delay.new(500, true),
+//             .reverseToLoop = reverseToLoop,
+//             .onReverse = false,
+//         };
+//     }
+//
+//     pub fn deinit(self: *const This) void {
+//         for (self.sprites.items) |sprite| {
+//             sprite.unload_texture();
+//         }
+//         self.sprites.deinit();
+//     }
+//
+//     pub fn nextSprite(self: *This) void {
+//         if (self.delay.onCooldown()) return;
+//
+//         if (self.onReverse) {
+//             self.applyReverse();
+//         } else {
+//             self.currentSprite += 1;
+//         }
+//
+//         const shouldReset = self.currentSprite >= self.sprites.items.len;
+//         const shouldReverse = !self.onReverse and shouldReset;
+//         if (self.reverseToLoop and shouldReverse) {
+//             self.onReverse = true;
+//             self.currentSprite -= 1;
+//         } else if (!self.reverseToLoop and shouldReset) self.currentSprite = 0;
+//
+//         self.delay.applyDelay();
+//     }
+//
+//     fn applyReverse(self: *This) void {
+//         if (self.currentSprite != 0) {
+//             self.currentSprite -= 1;
+//             return;
+//         }
+//         self.currentSprite = 0;
+//         self.onReverse = false;
+//     }
+// };
+//
+// pub const AnimationSpriteSheet = struct {
+//     const This = @This();
+//     spriteSheet: SpriteSheet,
+//     delay: Delay,
+//     count: usize,
+//     reverseToLoop: bool,
+//     onReverse: bool,
+//
+//     pub fn init(filepath: [:0]const u8, spriteSize: [2]usize, grid: [2]usize, padding: utils.Point, reverseToLoop: bool) This {
+//         const spriteSheet = SpriteSheet.load_sprite_sheet(filepath, spriteSize[0], spriteSize[1], grid[0], grid[1], padding);
+//
+//         return This{
+//             .spriteSheet = spriteSheet,
+//             .delay = Delay.new(500, true),
+//             .count = 0,
+//             .reverseToLoop = reverseToLoop,
+//             .onReverse = false,
+//         };
+//     }
+//
+//     pub fn deinit(self: *This) void {
+//         self.spriteSheet.unload_sprite_sheet();
+//     }
+//
+//     pub fn nextSprite(self: *This) void {
+//         if (self.delay.onCooldown()) return;
+//
+//         if (self.onReverse) {
+//             self.applyReverse();
+//         } else {
+//             self.count += 1;
+//         }
+//
+//         const shouldReset = self.count >= (self.spriteSheet.gridRows * self.spriteSheet.gridCols);
+//         const shouldReverse = !self.onReverse and shouldReset;
+//         if (self.reverseToLoop and shouldReverse) {
+//             self.onReverse = true;
+//             self.count -= 1;
+//         } else if (!self.reverseToLoop and shouldReset) self.count = 0;
+//
+//         self.delay.applyDelay();
+//     }
+//
+//     fn applyReverse(self: *This) void {
+//         if (self.count != 0) {
+//             self.count -= 1;
+//             return;
+//         }
+//
+//         self.count = 0;
+//         self.onReverse = false;
+//     }
+//
+//     pub fn draw(self: *const This, render: *const Render, position: utils.Point) void {
+//         const row = self.count / self.spriteSheet.gridRows;
+//         const col = self.count / self.spriteSheet.gridCols;
+//
+//         render.drawSpriteSheet(position, self.spriteSheet, row, col);
+//     }
+// };
