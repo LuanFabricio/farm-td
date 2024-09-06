2e23646be67b3e4ebce59569be8e0f95d271939f
LuanFabricio <luanlimaleite@hotmail.com>
2024-09-06 19:27:32 -0300

    feat: Adding save/load Entity data into/from a file and CacheEntity

--- src/core/entity.zig
+++ src/core/entity.zig
@@ -1,12 +1,52 @@
+const std = @import("std");
+const allocator = std.heap.page_allocator;
+
 const utils = @import("../utils/utils.zig");
 
 const Status = @import("status.zig").Status;
+
+pub const CacheEntity = struct {
+    const This = @This();
+
+    cache: std.AutoHashMap([]u8, Entity),
+
+    pub fn init() This {
+        return This{
+            .cache = std.AutoHashMap([]u8, Entity).init(allocator),
+        };
+    }
+
+    pub fn read(self: *This, file_path: []const u8) !Entity {
+        if (self.cache.get(file_path)) |value| return value;
+
+        const entity = try Entity.load(file_path);
+        try self.cache.put(file_path, entity);
+        return entity;
+    }
+
+    pub fn update(self: *This, file_path: []const u8, entity: Entity) !void {
+        try self.cache.put(file_path, entity);
+    }
+};
 
 pub const Entity = struct {
     const This = @This();
     cost: u32,
     defaultStatus: Status,
     status: Status,
+
+    pub fn load(file_path: []const u8) !This {
+        var buffer: [32]u8 = undefined;
+        _ = try std.fs.cwd().readFile(file_path, &buffer);
+        return @as(*This, @alignCast(@ptrCast(&buffer))).*;
+    }
+
+    pub fn save(self: *const This, file_path: []const u8) !void {
+        const file = try std.fs.cwd().createFile(file_path, .{ .read = true });
+        defer file.close();
+
+        try file.writeAll(std.mem.asBytes(self));
+    }
 
     pub fn copy(self: *This, other: This) void {
         self.cost = other.cost;
