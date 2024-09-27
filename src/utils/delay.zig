cc0a6c8fbc1f6c64d0fc4d354ace832cdaa5e290
LuanFabricio <luanlimaleite@hotmail.com>
2024-09-26 23:02:54 -0300

    feat: Adding random delay on Delay struct

--- src/utils/delay.zig
+++ src/utils/delay.zig
@@ -1,16 +1,19 @@
 const std = @import("std");
 const timestamp = std.time.milliTimestamp;
+const rand = std.crypto.random;
 
 pub const Delay = struct {
     const This = @This();
     delay: i64,
     timer: i64,
+    isRandom: bool,
 
     pub fn new(delay: i64, startOnCooldown: bool) This {
         const now = timestamp();
         return This{
             .delay = delay,
             .timer = now + if (startOnCooldown) delay else 0,
+            .isRandom = false,
         };
     }
 
@@ -22,5 +25,8 @@ pub const Delay = struct {
     pub fn applyDelay(self: *This) void {
         const now = timestamp();
         self.timer = now + self.delay;
+        if (self.isRandom) {
+            self.timer += rand.intRangeAtMost(i64, -2500, 150);
+        }
     }
 };
